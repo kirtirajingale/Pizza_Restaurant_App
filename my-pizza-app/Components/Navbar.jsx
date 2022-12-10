35dc8e0472bb28cd6a71e5a661b0f6eb4f2b90b3
Kirtiraj Ingale <kirtiraingale46@gmail.com>
2022-12-10 23:54:24 +0530

    styles added

--- my-pizza-app/Components/Navbar.jsx
+++ my-pizza-app/Components/Navbar.jsx
@@ -14,7 +14,18 @@ const Navbar = () => {
         <div className={styles.text}>+91-9454023332</div>
       </div>
       </div>
-      <div className={styles.item}>center</div>
+      <div className={styles.item}>
+          <ul className={styles.list}>
+            <li className={styles.listItem}>HomePage</li>
+            <li className={styles.listItem}>Products</li>
+            <li className={styles.listItem}>Menu</li>
+            <Image src="/img/logo.png" alt="logo" width={100} height={40}
+            />
+            <li className={styles.listItem}>Events</li>
+            <li className={styles.listItem}>Blog</li>
+            <li className={styles.listItem}>Contact</li>
+          </ul>
+      </div>
       <div className={styles.item}>Right</div>
     </div>
   )
